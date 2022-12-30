68ad862b80334ae8c1cc94b4b1ee8ef82aba6c10
TaeWoo92 <nike006@naver.com>
2022-12-30 09:20:53 +0900

    20221230

---
+++ vpc/main.tf
@@ -0,0 +1,35 @@
+provider "aws" {
+
+  region = "ap-northeast-2"
+}
+
+data "aws_vpc" "default" {
+
+  default = true
+}
+
+data "aws_subnets" "default" {
+
+  filter {
+    name   = "vpc-id"
+    values = [data.aws_vpc.default.id]
+  }
+}
+
+data "aws_subnet" "default" {
+
+  for_each = toset(data.aws_subnets.default.ids)
+  id    = each.value
+
+}
+
+
+output "vpc-id" {
+
+  value = data.aws_vpc.default.id
+}
+
+output "subnet-cidr_blocks" {
+
+  value = [for s in data.aws_subnet.default : s.cidr_block]
+}
