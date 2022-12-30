0a09b237542f75f7eec7b452972cfa24d38676b8
TaeWoo92 <nike006@naver.com>
2022-12-30 16:12:57 +0900

    20221230

---
+++ terraform-state/file-layout-example/global/s3/main.tf
@@ -0,0 +1,52 @@
+terraform {
+  required_version = ">= 1.0.0, < 2.0.0"
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = "ap-northeast-2"
+}
+
+resource "aws_s3_bucket" "terraform_state" {
+  bucket = "std06-terraform-state"
+
+  #  lifecycle {
+  #    prevent_destroy = true
+  #  }
+
+  force_destroy = true
+}
+resource "aws_s3_bucket_versioning" "terraform_versioning" {
+  bucket = aws_s3_bucket.terraform_state.id
+
+  versioning_configuration {
+    status = "Enabled"
+  }
+}
+resource "aws_s3_bucket_server_side_encryption_configuration" "terraform-encryption" {
+  bucket = aws_s3_bucket.terraform_state.bucket
+
+  rule {
+    apply_server_side_encryption_by_default {
+      sse_algorithm = "AES256"
+    }
+  }
+}
+
+
+resource "aws_dynamodb_table" "terraform_locks" {
+  name         = "std06-terraform-locks"
+  billing_mode = "PAY_PER_REQUEST"
+  hash_key     = "LockID"
+
+  attribute {
+    name = "LockID"
+    type = "S"
+  }
+}
--- web-cluster/main.tf
+++ web-cluster/main.tf
@@ -22,7 +22,7 @@ resource "aws_launch_template" "example" {
   key_name               = "std06-key"
   vpc_security_group_ids = [aws_security_group.instance.id]
 
-  user_data = "${base64encode(data.template_file.web_output.rendered)}"
+  user_data = base64encode(data.template_file.web_output.rendered)
 
   lifecycle {
     create_before_destroy = true
@@ -42,14 +42,56 @@ resource "aws_autoscaling_group" "example" {
     value               = "std06-terrform-asg-example"
     propagate_at_launch = true
   }
-
-
+  target_group_arns = [aws_lb_target_group.asg.arn]
+  health_check_type = "ELB"
 
   launch_template {
     id      = aws_launch_template.example.id
     version = "$Latest"
   }
 }
+
+resource "aws_lb" "example" {
+  name               = "std06-terraform-ags-example"
+  load_balancer_type = "application"
+  subnets            = data.aws_subnets.default.ids
+  security_groups    = [aws_security_group.alb.id]
+}
+
+# 로드밸런스리스너  구성
+resource "aws_lb_listener" "http" {
+  load_balancer_arn = aws_lb.example.arn
+  port              = 80
+  protocol          = "HTTP"
+
+  # 기본값으로 단순한 404 페이지 오류를 반환한
+  default_action {
+    type = "fixed-response"
+
+    fixed_response {
+      content_type = "text/plain"
+      message_body = "404: page not found"
+      status_code  = 404
+    }
+  }
+}
+
+resource "aws_lb_listener_rule" "ags" {
+  listener_arn = aws_lb_listener.http.arn
+  priority     = 100
+
+  condition {
+    path_pattern {
+      values = ["*"]
+    }
+  }
+
+  action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.asg.arn
+  }
+}
+
 
 resource "aws_security_group" "instance" {
   name = var.security_group_name
@@ -58,6 +100,44 @@ resource "aws_security_group" "instance" {
     from_port   = var.server_port
     to_port     = var.server_port
     protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+resource "aws_lb_target_group" "asg" {
+  name     = "std06-terraform-asg-example"
+  port     = var.server_port
+  protocol = "HTTP"
+  vpc_id   = data.aws_vpc.default.id
+
+  health_check {
+    path                = "/"
+    protocol            = "HTTP"
+    matcher             = "200"
+    interval            = 15
+    timeout             = 3
+    healthy_threshold   = 2
+    unhealthy_threshold = 2
+  }
+}
+resource "aws_security_group" "alb" {
+  name = "std06-terraform-example-alb"
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  # 모든 아웃바운드 트래픽 허용
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
     cidr_blocks = ["0.0.0.0/0"]
   }
 }
